96b543bc0fd1c3e6b03de6049017a1280a453369
Álvaro Morales <moralesalvaro97@gmail.com>
2025-01-22 17:04:15 +0100

    Tokenizer refactored.

--- tokenizer/tokenizer.c
+++ tokenizer/tokenizer.c
@@ -39,61 +39,69 @@ size_t	count_n_tokens(char *argv)
 	return (++n_op);
 }
 
-t_darray	*tokenizer_str(char *argv)
+int	tokenize_operator(char *line, int *i, int *start, t_darray **tokens)
+{
+	int	len_op;
+
+	len_op = 1;
+	if (*i != 0 && !isspace(line[*i - 1]) && !is_simple_operator(line[*i - 1]))
+	{
+		if (!add_token(tokens, ft_substr(line, *start, *i - *start)))
+			return (free_darray(*tokens), 0);
+	}
+	if (is_double_operator(line[*i]) && is_double_operator(line[*i + 1]))
+		len_op++;
+	if (!add_token(tokens, ft_substr(line, *i, len_op)))
+		return (free_darray(*tokens), 0);
+	if (is_double_operator(line[*i]) && is_double_operator(line[*i + 1]))
+		*i = *i + 1;
+	while (isspace(line[*i + 1]) && line[*i + 1] != '\0')
+		*i = *i + 1;
+	*i = *i + 1;
+	*start = *i;
+	return (1);
+}
+
+int	tokenize_literal(char *line, int *i, int *start, t_darray **tokens)
+{
+	if (!add_token(tokens, ft_substr(line, *start, *i - *start)))
+		return (free_darray(*tokens), 0);
+	while (isspace(line[*i]))
+		*i = *i + 1;
+	*start = *i;
+	*i = *i - 1;
+	return (1);
+}
+
+t_darray	*tokenizer_str(char *line)
 {
 	int			i;
-	int			inside_quote;
-	int			len_op;
 	int			start;
 	t_darray	*tokens;
 
 	i = 0;
 	start = 0;
-	inside_quote = 0;
-	tokens = alloc_darray(count_n_tokens(argv) + 1, sizeof(char *));
+	tokens = alloc_darray(count_n_tokens(line) + 1, sizeof(char *));
 	if (!tokens)
 		return (NULL);
-	while (argv[i])
+	while (tokens->full_idx < count_n_tokens(line))
 	{
-		len_op = 1;
-		while (still_in_quote(argv[i], '\''))
+		while (still_in_quote(line[i], '\''))
 			i++;
-		while (still_in_quote(argv[i], '\"'))
+		while (still_in_quote(line[i], '\"'))
 			i++;
-		if (is_simple_operator(argv[i]))
+		if (is_simple_operator(line[i]))
 		{
-			if (i != 0 && !isspace(argv[i - 1]) && !is_simple_operator(argv[i - 1]))
-			{
-				if (!add_token(&tokens, ft_substr(argv, start, i - start)))
-					return (free_darray(tokens), NULL);
-			}
-			if (is_double_operator(argv[i]) && is_double_operator(argv[i + 1]))
-				len_op++;
-			if (!add_token(&tokens, ft_substr(argv, i, len_op)))
-				return (free_darray(tokens), NULL);
-			if (is_double_operator(argv[i]) && is_double_operator(argv[i + 1]))
-				i++;
-			while (isspace(argv[i + 1]) && argv[i + 1] != '\0')
-				i++;
-			i++;
-			start = i;
+			if (!tokenize_operator(line, &i, &start, &tokens))
+				return (NULL);
 			continue ;
 		}
-		else if (isspace(argv[i]))
+		else if (isspace(line[i]) || !line[i])
 		{
-			if (!add_token(&tokens, ft_substr(argv, start, i - start)))
-				return (free_darray(tokens), NULL);
-			while (isspace(argv[i]))
-				i++;
-			start = i;
-			i--;
+			if (!tokenize_literal(line, &i, &start, &tokens))
+				return (NULL);
 		}
 		i++;
-	}
-	if (start != i)
-	{
-		if (!add_token(&tokens, ft_substr(argv, start, i - start)))
-			return (free_darray(tokens), NULL);
 	}
 	add_token(&tokens, NULL);
 	return (tokens);
@@ -124,36 +132,36 @@ t_token	*tokenizer_t_tokens(char **tokens_strings, size_t len)
 	return (token_stream);
 }
 
-int main(int argc, char **argv)
-{
-    char		*line;
-	int			i;
-	t_darray	*tokens_array;
-	char		**tokens_strings;
-	t_token		*token_stream;
+// int main(int argc, char **argv)
+// {
+//     char		*line;
+// 	int			i;
+// 	t_darray	*tokens_array;
+// 	char		**tokens_strings;
+// 	t_token		*token_stream;
 	
-	while (line = readline("\033[32mminishell\033[0m$ "))
-	{
-		ft_printf("\n");
-		add_history(line);
-		tokens_array = tokenizer_str(line);
-		tokens_strings = (char **) tokens_array->darray;
-		token_stream = tokenizer_t_tokens(tokens_strings, tokens_array->full_idx);
-		free(tokens_array);
+// 	while (line = readline("\033[32mminishell\033[0m$ "))
+// 	{
+// 		ft_printf("\n");
+// 		add_history(line);
+// 		tokens_array = tokenizer_str(line);
+// 		tokens_strings = (char **) tokens_array->darray;
+// 		token_stream = tokenizer_t_tokens(tokens_strings, tokens_array->full_idx);
+// 		free(tokens_array);
 		
-		i = 0;
-		while (token_stream[i].type != END)
-		{
-			printf("token: %s -|- type: %d\n", token_stream[i].text, token_stream[i].type);
-			i++;
-		}
-		ft_free_array(tokens_strings);
-		free(token_stream);
-		free(line);
-	}
-		rl_clear_history();
-		return 0;
-}
+// 		i = 0;
+// 		while (token_stream[i].type != END)
+// 		{
+// 			printf("token: %s -|- type: %d\n", token_stream[i].text, token_stream[i].type);
+// 			i++;
+// 		}
+// 		ft_free_array(tokens_strings);
+// 		free(token_stream);
+// 		free(line);
+// 	}
+// 		rl_clear_history();
+// 		return 0;
+// }
 
 
 
@@ -162,5 +170,6 @@ int main(int argc, char **argv)
 
 	ft_printf("%d\n", count_n_tokens("\"Hola que tal \' ls Me vengo\" \'"));
 	ft_printf("%d\n", count_n_tokens("\"Hola que tal \' ls Me vengo\"\'"));
+	Putting | <out fire >with_gasoline
 
  */
