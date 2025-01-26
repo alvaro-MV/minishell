50dcfce33d3d3d580739fb14736441c10e46f4ce
alvaro-MV <moralesalvaro97@gmail.com>
2025-01-26 16:53:48 +0100

    expand_double_quoted function changed.

--- tokenizer/tokenizer.h
+++ tokenizer/tokenizer.h
@@ -4,7 +4,6 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <readline/readline.h>
-# include <editline/readline.h>
 # include <readline/history.h>
 # include "../data_structs/dynamic_array.h"
 # include "../libft/include/libft.h"
