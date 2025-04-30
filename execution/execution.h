de28e03300d0e429135f1875eb53d99364206477
Alvaro Morales De Vera <alvmoral@c2r8s4.42madrid.com>
2025-04-30 19:20:46 +0200

    para revertir con loreto.

--- execution/execution.h
+++ execution/execution.h
@@ -13,13 +13,13 @@ typedef struct s_exec
 }					t_exec;
 
 int		executor(t_cmd_pipe *sequence, t_dictionary *env, char **main_env);
-int		execute_io_redir(t_exec *exec);
+int		execute_io_redir(t_exec exec);
 int		is_builtin(char **cmd_args);
 int		run_builtin(t_exec *exec);
-int		execute_child(t_exec *exec_vars);
+int		execute_child(t_exec exec_vars);
 void	close_cmd_fds(t_cmd *cmd);
-int		here_doc(char *delimiter, t_exec *exec);
-int		traverse_io_redir(t_io_redir *ix, t_exec *exec);
+int		here_doc(char *delimiter, t_dictionary *env, int saved_stdin);
+int		traverse_io_redir(t_io_redir *ix, t_exec exec);
 char	**create_args(t_cmd *cmd);
 
 #endif
